48e929c439bb6649e5267c8ad70b20b97830e802
kryturek <75915136+kryturek@users.noreply.github.com>
2023-04-30 17:25:59 +0100

    list of parts of speech added

--- components/VocabScreen.js
+++ components/VocabScreen.js
@@ -1,15 +1,76 @@
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Dimensions } from "react-native";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	StyleSheet,
+	FlatList,
+} from "react-native";
 
-export default function VocabScreen() {
+const partsOfSpeech = [
+	"nouns",
+	"adjectives",
+	"adverbs",
+	"prepositions",
+	"conjunctions",
+	"interjections",
+];
+
+
+
+const VocabScreen = () => {
+
+	const [backgroundColor, setBackgroundColor] = useState({
+		nouns: 'red',
+		adjectives: 'yellow',
+		adverbs: 'orange',
+		prepositions: 'green',
+		conjunctions: 'purple',
+		interjections: 'gray'
+	 });
+
+	const renderItem = ({ item }) => {
+		const textColor = item === 'adjectives' ? 'black' : 'white';
+
+		return (
+		  <TouchableOpacity style={[styles.item, { backgroundColor: backgroundColor[item] }]}>
+			 <Text style={[styles.title, { color: textColor} ]}>{item}</Text>
+		  </TouchableOpacity>
+		);
+	 };
+
 	return (
 		<View style={styles.container}>
-			<Text>Vocabulary screen</Text>
+			<FlatList
+				data={partsOfSpeech}
+				renderItem={renderItem}
+				keyExtractor={(item) => item}
+				contentContainerStyle={styles.flatListContainer}
+				scrollEnabled={false}
+			/>
 		</View>
-	)
-}
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
-		padding: 50,
+		flex: 1,
+	},
+	item: {
+		height: 100,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	flatListContainer: {
+		flexGrow: 1,
+		justifyContent: 'space-evenly'
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		
 	}
-})
+});
+
+export default VocabScreen;
