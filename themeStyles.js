80c888c12dff8cc352eddfb6a9d165b8767cbff9
kryturek <75915136+kryturek@users.noreply.github.com>
2023-05-06 04:53:11 +0100

    global styles for consistency

---
+++ themeStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const jet = '#39393a';
+const platinum = "#e6e6e6";
+const coral = "#ff8552";
+const purple = '#aa22aa';
+
+const themeStyles = StyleSheet.create({
+	jetBackground: {
+		backgroundColor: '#39393a',
+		color: "#e6e6e6"
+	},
+	platinumBackground: {
+		backgroundColor: "#e6e6e6",
+		color: '#39393a'
+	}
+})
+
+export default themeStyles;
